e8219760cea9295fdd3f687f49cba6ede35476e6
Kyle Richnafsky <7krich@gmail.com>
2022-06-29 14:53:11 -0400

    trying to resolve circular dependency errors and jsx errors

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/react-hooks";
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -19,7 +18,7 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
-
+  
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,11 @@ const resolvers = {
     },
   
     Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create({ args });
+        const token = signToken(user);
+        return { token, user };
+      },
       login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
   
@@ -28,11 +33,6 @@ const resolvers = {
           throw new AuthenticationError('Incorrect credentials');
         }
   
-        const token = signToken(user);
-        return { token, user };
-      },
-      addUser: async (parent, args) => {
-        const user = await User.create({ args });
         const token = signToken(user);
         return { token, user };
       },
